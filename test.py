7e0f7f9a4ad61ae38c92562e0d46da04bf9d3ad0
Pr0stoy <prostakegor@gmail.com>
2026-08-01 16:21:04 +0300

    sortdone

--- test.py
+++ test.py
@@ -1,3 +1,18 @@
-var = 0
-print(var > 0)
-print(not(var <= 0))
+my_list = []
+length = int(input("How many elements do you want to sort? : "))
+for i in range(length):
+      el = int(input("Add number to the list : "))
+      my_list.append(el)
+print(my_list)
+  # It's a little fake, we need it to enter the while loop.
+def fun(x):
+    swapped = True
+    while swapped:
+        swapped = False  # no swaps so far
+        for i in range(len(x)-1):
+            if x[i] > x[i + 1]:
+                swapped = True  # a swap occurred!
+                x[i], x[i + 1] = x[i + 1], x[i]
+    
+    return x
+print(fun(my_list))
