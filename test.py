f0aa8106684cf20c4715032b8b39b567ea46e620
Pr0stoy <prostakegor@gmail.com>
2026-08-16 18:17:45 +0300

    s

--- test.py
+++ test.py
@@ -1,14 +1,8 @@
-class Enemy:
-    def __init__(self,enemy_id):
-        self.id = enemy_id
-        self.hp = 100
-    def __repr__(self):
-        return f"Enemy {self.id} has {self.hp} hp"
-    def take_damage(self,amount_damage):
-        self.hp -= amount_damage
-        if self.hp <= 0:
-            print(f"Enemy{self.id} is dead")
-enemies = [Enemy(i) for i in range(1,11)]
-print(enemies)
-enemies[3].take_damage(60)
-print(enemies)
+def check_temps(temperatures):
+    for i in temperatures:
+        if i < 0:
+            return True
+    return False
+
+print(check_temps([12, 15, 8, -2, 5]))  # Should print True
+print(check_temps([10, 14, 22, 18]))   # Should print False
